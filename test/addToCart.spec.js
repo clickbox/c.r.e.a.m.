661e64bf042ec6e79fd78553f4e45f3f1220f259
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-22 16:33:19 -0500

    failing add to cart test

--- test/addToCart.spec.js
+++ test/addToCart.spec.js
@@ -1,11 +1,12 @@
 import { Product } from '../modules/products'
+// import * as add from '../modules/add'
 
-it('put product in cart', async () => {    
+it('put product in cart', async () => {
     const product1 = await new Product('Ground Beef', 5.00)
     const product2 = await new Product('Apple', 1.00)
     await add.toCart(product1)
     await add.toCart(product2)
-    // await expect(session.cart[1].name).toBe('Apple')
-    // await expect(session.cart[1].price).toBe(1.00)
+    await expect(session.cart[1].name).toBe('Apple')
+    await expect(session.cart[1].price).toBe(1.00)
     console.log(add.cart)
 })
