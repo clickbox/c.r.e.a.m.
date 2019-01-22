44d2eeb5f7ac61b47e2d517937d6988f07d9ffe3
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-22 13:41:23 -0500

    remove semicolons

--- test/product.spec.js
+++ test/product.spec.js
@@ -1,29 +1,29 @@
-import { Product } from '../modules/products';
-import { BulkProduct } from '../modules/bulkProduct';
+import { Product } from '../modules/products'
+import { BulkProduct } from '../modules/bulkProduct'
 
 it('add product works', async () => {
-    const cart = [];
-    const newProduct = await new Product('Ground Beef');
-    await cart.push(newProduct);
-    const product = await cart[0].name;
-    await expect(product).toEqual('Ground Beef');
-});
+    const cart = []
+    const newProduct = await new Product('Ground Beef')
+    await cart.push(newProduct)
+    const product = await cart[0].name
+    await expect(product).toEqual('Ground Beef')
+})
 
 it('add product price', async () => {
-    const newProduct = await new Product('Ground Beef', 5.00);
-    await expect(newProduct.price).toEqual(5.00);
-});
+    const newProduct = await new Product('Ground Beef', 5.00)
+    await expect(newProduct.price).toEqual(5.00)
+})
 
 it('product is added to cart with price and name', async () => {
-    const cart = [];
-    const newProduct = await new Product('Ground Beef', 5.00);
-    await cart.push(newProduct);
-    await expect(cart[0].name).toBe('Ground Beef');
-    await expect(cart[0].price).toEqual(5.00);
-});
+    const cart = []
+    const newProduct = await new Product('Ground Beef', 5.00)
+    await cart.push(newProduct)
+    await expect(cart[0].name).toBe('Ground Beef')
+    await expect(cart[0].price).toEqual(5.00)
+})
 
 it('add product with weight for bulk price by pounds', async () => {
-    const newBulkProduct = await new BulkProduct('Ground Beef', 5.00, 2);
-    await expect(newBulkProduct.weight).toEqual(2);
-    await console.log(newBulkProduct);
+    const newBulkProduct = await new BulkProduct('Ground Beef', 5.00, 2)
+    await expect(newBulkProduct.weight).toEqual(2)
+    await console.log(newBulkProduct)
 })
