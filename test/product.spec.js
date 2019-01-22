0446c7396263a8922bbef70a31efa4ae25735441
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-21 23:09:01 -0500

    product price test

--- test/product.spec.js
+++ test/product.spec.js
@@ -6,4 +6,9 @@ it('add product works', async () => {
     await cart.push(newProduct);
     const product = await cart[0].name;
     await expect(product).toEqual('Ground Beef');
-}) 
+});
+
+it('add product price', async () => {
+    const newProduct = await new Product('Ground Beef', 5.00);
+    await expect(newProduct[1]).toEqual(5.00);
+});
