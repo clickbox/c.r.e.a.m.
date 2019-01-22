41640d742f17daa1ae86c57cc5a83c2eb4b7cc0f
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-22 13:39:24 -0500

    add bulkprice subclass to product class with weight

---
+++ modules/bulkProduct.js
@@ -0,0 +1,8 @@
+import { Product } from './products'
+
+export class BulkProduct extends Product {
+    constructor(name, price, weight){
+        super(name, price);
+        this.weight = weight;
+    }
+}
