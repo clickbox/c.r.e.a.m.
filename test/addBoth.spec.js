62c44c6ba6d9ab2f62b0d339af904edfb4426c09
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-22 20:43:09 -0500

    seperate last test to isolate scope

---
+++ test/addBoth.spec.js
@@ -0,0 +1,17 @@
+import { Product } from '../modules/products'
+import * as add from '../modules/add'
+import { BulkProduct } from '../modules/bulkProduct'
+
+it('should add both type of product to cart', async () => {
+    const cart = add.cart;
+    const product1 = await new BulkProduct('Ground Beef', 5.00, 5)
+    const product2 = await new Product('Apple', 1.00)
+    await add.toCart(product1)
+    await add.toCart(product2)
+    await expect(cart[1].name).toBe('Apple')
+    await expect(cart[1].price).toEqual(1)
+    await expect(cart[0].name).toBe('Ground Beef')
+    await expect(cart[0].price).toEqual(5.00)
+    await expect(cart[0].weight).toEqual(5)
+    console.log(cart)
+})
