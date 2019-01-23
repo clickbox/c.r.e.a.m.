c0a65a8c8f645261888ef9616435708ac94b6ad1
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-23 00:49:20 -0500

    update test

--- test/setBulkPrice.spec.js
+++ test/setBulkPrice.spec.js
@@ -3,8 +3,7 @@ import * as add from '../modules/add'
 import * as calculate from '../modules/calculate'
 
 it('should calculate price of weighed products', async () => {
-    const cart = add.cart
-    const product = await new BulkProduct('Ground Beef', 5.00, 2)
+    let product = await new BulkProduct('Ground Beef', 5.00, 2)
     product.price = await calculate.bulkPrice(product);
     await expect(product.price).toBe(10)
     console.log(product)
