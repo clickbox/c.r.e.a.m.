9535486b992d455ff82299d7e32cd682f0f2e6f8
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-23 01:02:33 -0500

    remove semicolon

--- test/setBulkPrice.spec.js
+++ test/setBulkPrice.spec.js
@@ -3,7 +3,7 @@ import * as calculate from '../modules/calculate'
 
 it('should calculate price of weighed products', async () => {
     let product = await new BulkProduct('Ground Beef', 5.00, 2)
-    await calculate.bulkPrice(product);
+    await calculate.bulkPrice(product)
     await expect(product.price).toBe(10)
     console.log(product)
 })
