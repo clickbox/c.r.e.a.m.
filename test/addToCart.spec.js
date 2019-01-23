9e24cbd2d0c505cbe67bccc3cacbf68f42ed5af7
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-22 20:18:34 -0500

    add weight to Ground Beef

--- test/addToCart.spec.js
+++ test/addToCart.spec.js
@@ -13,7 +13,7 @@ it('put product in cart', async () => {
 })
 
 it('should add both type of product to cart', async () => {
-    const product1 = await new BulkProduct('Ground Beef', 5.00)
+    const product1 = await new BulkProduct('Ground Beef', 5.00, 5)
     const product2 = await new Product('Apple', 1.00)
     await add.toCart(product1)
     await add.toCart(product2)
