744a94caac7036a79b61e1f68bc67e66d5358144
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-23 16:16:10 -0500

    remove product by line item test

--- test/removeProduct.spec.js
+++ test/removeProduct.spec.js
@@ -27,4 +27,19 @@ it('should remove first product in cart', async () => {
     await expect(inventory[0].name).toBe('Lime')
 })
 
+it('should remove first product in cart', async () => {
+    let id = await 1
+    let cart = add.cart
+    const apple = await new Product('Apple', 1.00)
+    const lime = await new Product('Lime', .50)
+    const orange = await new Product('Orange', 1.50)
+    await add.toCart(apple, lime, orange)
+    await expect(cart.length).toEqual(3)
+    await expect(cart[1].name).toBe('Lime')
+    await cart.remove.lineItem(id)
+    await expect(cart.length).toEqual(2)
+    await expect(cart[2].name).toBeUndefined()
+    await expect(cart[1].name).toBe('Orange')
+})
+
 
