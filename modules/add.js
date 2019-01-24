17948f52056cb71d4acaf2b151b3673409d05de9
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-24 16:31:32 -0500

    add toDiscounts function

--- modules/add.js
+++ modules/add.js
@@ -1,5 +1,10 @@
 export const cart = []
+export const discounts = []
 
 export function toCart(product){
   return product ? cart.push(product) : console.log('Product Not Found')
+}
+
+export function toDiscounts(discount) {
+  return discount ? discounts.push(discount) : console('Discount Unavailable')
 }
