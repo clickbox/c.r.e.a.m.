eb1b404c8baf61b02a40aaa069f7e380245cd607
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-24 15:40:49 -0500

    add to discounts array test

---
+++ test/addToDiscounts.spec.js
@@ -0,0 +1,9 @@
+import { Discount } from '../modules/discount'
+import * as add from '../modules/add'
+
+it('should add discount to discounts array', async () => {
+    let discounts = add.discounts
+    const Buy1Get1Free = await new Discount(1, 1, 0, 100)
+    await add.toDiscounts(Buy1Get1Free)
+    await expect.any(discounts)
+})
