9e4bb4dcd63c3adce5c36bbc2adae36ba4ce1042
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-24 00:08:29 -0500

    add discount class module

---
+++ modules/discount.js
@@ -0,0 +1,8 @@
+export class Discount {
+    constructor(numToBuy, numOnSale, limit, percentOff) {
+        this.numToBuy = numToBuy
+        this.numOnSale = numOnSale
+        this.limit = limit
+        this.percentOff = percentOff
+    }
+}
