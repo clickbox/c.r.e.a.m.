6218e6049f05360915599e073a2aedf7ed0e2bdb
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-24 23:22:33 -0500

    refactor add to cart test

--- test/addToCart.spec.js
+++ test/addToCart.spec.js
@@ -4,9 +4,14 @@ import * as add from '../modules/add'
 it('put product in cart', async () => {
     let cart = add.cart
     const apple = await new Product('Apple', 1.00)
+    const apple2 = await new Product('Apple', 1.00, 3)
     await add.toCart(apple)
     await expect(cart[0].name).toBe('Apple')
     await expect(cart[0].quantity).toEqual(1)
     await add.toCart(apple)
     await expect(cart[0].quantity).toEqual(2)
+    await expect(cart.length).toEqual(1)
+    await add.toCart(apple2)
+    await expect(cart[0].quantity).toEqual(5)
+    console.log(cart)
 })
