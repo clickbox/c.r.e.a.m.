771f2165ff3f2729536036b876a628d29922eff0
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-24 21:42:47 -0500

    update bulkProduct price

--- modules/bulkProduct.js
+++ modules/bulkProduct.js
@@ -3,6 +3,7 @@ import { Product } from './products'
 export class BulkProduct extends Product {
     constructor(name, price, weight){
         super(name, price)
+        this.price = price * weight
         this.weight = weight
     }
 }
