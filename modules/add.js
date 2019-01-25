6992f5c44f19d9658a99bcb5f8707f6e1271b490
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-24 23:09:58 -0500

    add to cart test

--- modules/add.js
+++ modules/add.js
@@ -1,8 +1,8 @@
 export const cart = []
 export const discounts = []
-
 export function toCart(product){
-  return product ? cart.push(product) : console.log('Product Not Found')
+  let inCart = cart.find(cart => cart.name === product.name)
+  inCart ? inCart.quantity += product.quantity : cart.push(product)
 }
 
 export function toDiscounts(discount) {
