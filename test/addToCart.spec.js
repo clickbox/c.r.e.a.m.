269f22db9289e1ee26d86591ad7e311ded1f76e5
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-24 22:31:20 -0500

    add to cart test

--- test/addToCart.spec.js
+++ test/addToCart.spec.js
@@ -2,11 +2,11 @@ import { Product } from '../modules/products'
 import * as add from '../modules/add'
 
 it('put product in cart', async () => {
-    const product1 = await new Product('Ground Beef', 5.00)
-    const product2 = await new Product('Apple', 1.00)
-    await add.toCart(product1)
-    await add.toCart(product2)
-    await expect(add.cart[1].name).toBe('Apple')
-    await expect(add.cart[1].price).toBe(1.00)
+    let cart = add.cart
+    const apple = await new Product('Apple', 1.00)
+    await add.toCart(apple)
+    await expect(cart[0].name).toBe('Apple')
+    await expect(cart[0].quantity).toEqual(1)
+    await add.toCart(apple)
+    await expect(cart[0].quantity).toEqual(2)
 })
-
