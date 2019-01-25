b350641a90d04eb8f8667c97a180bd8b275b46fb
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-24 21:40:12 -0500

    set bulk price test

--- test/setBulkPrice.spec.js
+++ test/setBulkPrice.spec.js
@@ -3,6 +3,10 @@ import * as calculate from '../modules/calculate'
 
 it('should calculate price of weighed products', async () => {
     let product = await new BulkProduct('Ground Beef', 5.00, 2)
-    await calculate.bulkPrice(product)
     await expect(product.price).toBe(10)
+})
+
+it('should calculate price in constructor', async () => {
+    let peanuts = await new BulkProduct('Peanuts', 2.50, 4)
+    await expect(peanuts.price).toEqual(10)
 })
