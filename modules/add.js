e7b18b0c6078d3c10fd29ec4c6fa18961fc00bbf
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-26 18:28:41 -0500

    format change

--- modules/add.js
+++ modules/add.js
@@ -1,4 +1,5 @@
 export const cart = []
+
 export function toCart(product){
   let inCart = cart.find(cart => cart.name === product.name)
   inCart ? inCart.quantity += product.quantity : cart.push(product)
