d8556613c4bd758aaef4e45614358a37ded8cfb0
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-26 15:07:24 -0500

    apply discount method on product

--- modules/products.js
+++ modules/products.js
@@ -1,3 +1,5 @@
+import { Buy1Get1Free } from "./buy1get1free";
+
 export class Product {
     constructor(name, price, quantity) {
         this.name = name
@@ -8,6 +10,20 @@ export class Product {
         this.onSaleQuantity = null
         this.discount = []
         this.addDiscount = (discount) => this.discount.push(discount)
+        this.applyDiscount = (item) => {
+            this.discount.forEach(discount => {
+                if (discount instanceof Buy1Get1Free) {
+                    if (item.quantity >= discount.numberToBuy 
+                        && item.saleCounter < discount.limit) {
+                            item.totalPrice = item.quantity * item.price - item.price * discount.numberOff
+                            item.onSaleQuantity += discount.numberOff
+                            item.saleCounter++
+                    } else {
+                        console.log('Error! Discount Does Not Apply')
+                    }
+                }
+            })
+        }
     }
 }
 
