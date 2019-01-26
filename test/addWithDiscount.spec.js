896b4b22db4024e91d478845016b4594e2e90a7e
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-26 18:19:25 -0500

    format change

--- test/addWithDiscount.spec.js
+++ test/addWithDiscount.spec.js
@@ -9,7 +9,6 @@ it('should get total with product that has discount', async () => {
     const peach = await new Product('Peach', .50, 2)
     const watermelon = await new Product('Watermelon', 1.50, 2)
     const buy1Get1Free = await new Buy1Get1Free(2, 1, 1)
-    
     await add.toCart(grape)
     await add.toCart(peach)
     await add.toCart(watermelon)
