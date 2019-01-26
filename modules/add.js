bc9493d686d2cc7a7449a084f42a6fcf853dc6ee
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-26 00:23:43 -0500

    remove discount

--- modules/add.js
+++ modules/add.js
@@ -1,10 +1,5 @@
 export const cart = []
-export const discounts = []
 export function toCart(product){
   let inCart = cart.find(cart => cart.name === product.name)
   inCart ? inCart.quantity += product.quantity : cart.push(product)
-}
-
-export function toDiscounts(discount) {
-  return discount ? discounts.push(discount) : console('Discount Unavailable')
 }
