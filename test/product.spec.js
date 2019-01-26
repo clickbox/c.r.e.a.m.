d0adfb0a8b27360a11c74d09f50a4dff6a9e5969
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-25 19:58:51 -0500

    saleCounter test

--- test/product.spec.js
+++ test/product.spec.js
@@ -47,3 +47,8 @@ it('should have a total price property', async () => {
     const blueberry = await new Product('Blueberry', 1.00, 4)
     await expect(blueberry.totalPrice).toEqual(null)
 })
+
+it('should have saleCounter prop default of 0', async () => {
+    const blueberry = await new Product('Blueberry', 1.00, 4)
+    await expect(blueberry.saleCounter).toEqual(0)
+})
