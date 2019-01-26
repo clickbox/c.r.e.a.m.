225d6b44036e6cf9669eda2339e1775165cb085d
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-26 16:58:52 -0500

    remove bulkprice calculator

--- modules/calculate.js
+++ modules/calculate.js
@@ -1,7 +1,3 @@
-export function bulkPrice(product){
-    product.price = product.price * product.weight 
-}
-
 export function ID(id, inventory){
     return inventory[id] ? inventory[id] : 'Product Not Found!'
 }
