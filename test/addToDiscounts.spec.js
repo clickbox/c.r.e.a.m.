875702ef8112df39cc106a1f76ba6392690c85fd
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-26 15:15:44 -0500

    refactor test

--- test/addToDiscounts.spec.js
+++ test/addToDiscounts.spec.js
@@ -3,11 +3,12 @@ import { Product } from '../modules/products'
 import * as add from '../modules/add'
 
 it('should add discount to discounts array', async () => {
+    let cart = add.cart
     const Jeep = await new Product('Jeep', 80000, 2)
-    await add.toCart(Jeep)
     await expect(Jeep.quantity).toEqual(2)
     const buy1Get1Free = await new Buy1Get1Free(2, 1, 1)
     await Jeep.addDiscount(buy1Get1Free)
     await Jeep.applyDiscount(Jeep)
-    console.log(Jeep)
+    await add.toCart(Jeep)
+    console.log(cart)
 })
