b42295de36133021ac44cecd6b622c1824dfc519
Ezenwa Isiogu <yo@ezenwa.com>
2019-01-26 16:53:50 -0500

    add discounts function to calculate module

--- modules/calculate.js
+++ modules/calculate.js
@@ -6,6 +6,12 @@ export function ID(id, inventory){
     return inventory[id] ? inventory[id] : 'Product Not Found!'
 }
 
+export function discounts(cart) {
+    cart.forEach(product => {
+        product.applyDiscount(product)
+    })
+}
+
 export function theTotal(cart){
     return cart.reduce(function(total, product){
       return total + ((product.price * product.quantity) + (product.totalPrice = null ? product.totalPrice : 0))
